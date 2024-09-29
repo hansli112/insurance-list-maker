ac82964b87dd48cf3ca558fcd3508607be576a77
hansli112 <lixuehan@lixuehandeMacBook-Pro.local>
2024-09-29 23:40:41 +0800

    Refactor: delivery message with DataFrame in app

--- app/app.py
+++ app/app.py
@@ -1,25 +1,33 @@
 import streamlit as st
+import pandas as pd
 from insurancer import Insurancer
 
 def main():
     st.title("保險名單")
 
-    insurancer = Insurancer()
+    # input prompt
+    insurancer = Insurancer("database.csv")
     insurancer.activity = st.text_input("請輸入活動名稱")
     insurancer.date = st.text_input("請輸入活動日期 (e.g. 2024.01.01)")
 
+    # select attendants
     attendants = []
     with st.container(height=400):
-        for name in insurancer.data.keys():
-            if st.checkbox(name):
+        for name, note in zip(insurancer.data[insurancer.columns[0]], insurancer.data["備註"]):
+            label = name if note == "" else f"{name} ({note})"
+            if st.checkbox(label):
                 attendants.append(name)
 
-    insure_list = ""
+    # make dataframe
+    insurance_df = pd.DataFrame(columns=insurancer.data.columns)
     for name in attendants:
-        insure_list += ','.join([name, *insurancer.data[name]]) + '\n'
+        info = insurancer.data[insurancer.data["姓名"] == name]
+        insurance_df = pd.concat([insurance_df, info], ignore_index=True)
 
+    # output csv file
     filename = f"{insurancer.date} {insurancer.activity}.csv"
-    st.download_button("下載名單", data=insure_list, file_name=filename)
+    insurance_csv = insurance_df.to_csv(columns=insurancer.columns, index=False)
+    st.download_button("下載名單", data=insurance_csv, file_name=filename)
 
 if __name__ == "__main__":
     main()
