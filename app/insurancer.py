830579cf1e2568ae91ed30af8827125a602c40f8
hansli112 <lixuehan@lixuehandeMacBook-Pro.local>
2024-09-29 21:19:42 +0800

    Refactor: Pass and store infomation with DataFrame

--- app/insurancer.py
+++ app/insurancer.py
@@ -4,12 +4,12 @@ from argparse import ArgumentParser
 from datetime import date
 from pathlib import Path
 
-from utils.id_checker import IDChecker
+from id_checker import IDChecker
 
 class Insurancer():
-    def __init__(self, database: str = "data.json"):
-        with open(database) as f:
-            self.data = json.load(f)
+    def __init__(self, database: str):
+        self.database_path = database
+        self.data = pd.read_csv(database, keep_default_na=False)
         self.columns = ["姓名", "身分證字號", "生日", "保額", "國籍", "性別"]
         self.id_checker = IDChecker()
         self.activity = None
@@ -23,23 +23,32 @@ class Insurancer():
 
     def make(self):
         # collect information
-        out = []
+        out = pd.DataFrame(columns=self.data.columns)
         for name in self.names:
-            if name not in self.data.keys():
+            info = self.data[self.data["姓名"] == name]
+            if info.empty:
                 info = self.make_info(name)
-                self.data[name] = info
-            out.append([name, *self.data[name]])
+                self.data = pd.concat((self.data, info), ignore_index=True)
+            out = pd.concat([out, info], ignore_index=True)
 
         # update the dataset
-        with open(args.database, 'w', encoding="utf-8") as f:
-            json.dump(self.data, f, indent=4, ensure_ascii=False)
+        self.data.to_csv(self.database_path, index=False)
 
         # output to csv file
         out_df = pd.DataFrame(data=out, columns=self.columns)
         out_df = out_df.sort_values(by="生日", key=lambda infos: [date(*[int(n) for n in info.split('/')]) for info in infos])
         out_df.to_csv(f"{self.date} {self.activity}.csv", index=False)
+        print(out)
 
     def make_info(self, name: str):
+        def _to_gender(id_number: str) -> str:
+            if id_number[1] == '1':
+                return 'M'
+            elif id_number[1] == '2':
+                return 'F'
+            else:
+                return None
+
         # ID number
         while True:
             print(f"新人員：{name}")
@@ -66,27 +75,18 @@ class Insurancer():
 
         # gender
         while True:
-            gender = self._to_gender(id_number) if not is_id else input("非本國籍，請輸入性別 M / F：").upper()
+            gender = _to_gender(id_number) if not is_id else input("非本國籍，請輸入性別 M / F：").upper()
             if gender in ["M", "F"]:
                 break
             print("性別錯誤，請重新輸入")
 
-        info = (id_number, birthday, str(100), country, gender)
+        info = pd.DataFrame(data=[[name, id_number, birthday, str(100), country, gender]], columns=self.columns)
         return info
     
-    def _to_gender(self, id_number: str) -> str:
-        if id_number[1] == '1':
-            return 'M'
-        elif id_number[1] == '2':
-            return 'F'
-        else:
-            return None
-
-
 def parse_args():
     parser = ArgumentParser()
     parser.add_argument("input_path", type=Path)
-    parser.add_argument("--database", type=Path, default="data.json")
+    parser.add_argument("--database", type=Path, default="database.csv")
     args = parser.parse_args()
     return args
 
